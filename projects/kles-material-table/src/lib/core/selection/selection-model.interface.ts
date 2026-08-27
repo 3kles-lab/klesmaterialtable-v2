284dbcd78738f6e6bb921da9c6a9fc0a828c47e5
Charles Da Silva Costa <CDASILVA@3KLES.local>
2026-08-27 14:34:38 +0200

    fix: clear selection model interface

--- projects/kles-material-table/src/lib/core/selection/selection-model.interface.ts
+++ projects/kles-material-table/src/lib/core/selection/selection-model.interface.ts
@@ -1,20 +1,24 @@
+import { Signal } from '@angular/core';
 import { Observable } from 'rxjs';
 import { KlesSelectionModelState } from './selection-state.enum';
 
 export interface IKlesSelectionModel<T> {
+    readonly count: Signal<number>;
+    readonly changed: Observable<{ added?: T[]; removed?: T[]; count: number; state: KlesSelectionModelState }>;
+    readonly stateChanged: Observable<KlesSelectionModelState>;
+    readonly selected: { items: T[]; count: number; state: KlesSelectionModelState };
+    readonly state: KlesSelectionModelState;
+    readonly selectionMode: boolean;
+
     select(value: T | T[], options?: { emitEvent?: boolean }): void;
     deselect(value: T | T[], options?: { emitEvent?: boolean }): void;
     toggle(value: T, options?: { emitEvent?: boolean }): void;
     reset(values: T[], options?: { emitEvent?: boolean }): void;
+    isSelected(value: T): boolean;
+    isMultipleSelection(): boolean;
     isEmpty(): boolean;
     hasValue(): boolean;
 
     disable(): void;
     enable(): void;
-
-    get selected(): { items?: T[]; count: number; state: KlesSelectionModelState };
-    get changed(): Observable<{ added?: T[]; removed?: T[]; count: number; state: KlesSelectionModelState }>;
-    get state(): KlesSelectionModelState;
-
-    get selectionMode(): boolean;
 }
